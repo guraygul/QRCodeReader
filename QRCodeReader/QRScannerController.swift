828d528d01426960962a623f81ec54389cb1ef2f
Güray Gül <guraygul59@gmail.com>
2024-02-11 20:40:15 +0300

    Initial Commit

---
+++ QRCodeReader/QRScannerController.swift
@@ -0,0 +1,27 @@
+//
+//  QRScannerController.swift
+//  QRCodeReader
+//
+//  Created by Simon Ng on 13/10/2021.
+//  Copyright © 2022 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerController: UIViewController {
+
+    @IBOutlet var messageLabel: UILabel!
+    @IBOutlet var topbar: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
